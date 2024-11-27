911ea369ca9a417b4624cce16cf0d4a36bf4f8f8
Vee Pabuence Vargas <beepabuence@gmail.com>
2024-11-26 21:01:16 -0500

    Reanimated and gesture handler added

--- src/views/InfoAPOD/InfoAPOD.jsx
+++ src/views/InfoAPOD/InfoAPOD.jsx
@@ -1,22 +1,45 @@
-import { View, Text, SafeAreaView, Image, ScrollView } from 'react-native';
+import {View, Text, SafeAreaView, Image, ScrollView} from 'react-native';
 import React from 'react';
-import { styles } from './styles';
+import {styles} from './styles';
+import {Gesture, GestureDetector, GestureHandlerRootView} from 'react-native-gesture-handler';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
 
 const InfoAPOD = ({route}) => {
-    const {title, url, hdurl, date, explanation, copyright} = route.params;
+  const {title, url, hdurl, date, explanation, copyright} = route.params;
+  const scale = useSharedValue(1);
+  const savedScale = useSharedValue(1);
 
-    return (
-    <ScrollView style={styles.container}>
+  const pinchGesture = Gesture.Pinch()
+    .onUpdate(e => {
+      scale.value = savedScale.value * e.scale;
+    })
+    .onEnd(() => {
+      savedScale.value = scale.value;
+    });
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{scale: scale.value}],
+  }));
+
+  return (
+    <GestureHandlerRootView style={{flex: 1}}>
+      <ScrollView style={styles.container}>
         <View style={styles.containerImage}>
-            <Image source={{uri: hdurl}} style={styles.image}/>
+          <GestureDetector gesture={pinchGesture}>
+              <Animated.Image source={{uri: url}} style={[styles.image, animatedStyle]} />
+          </GestureDetector>
         </View>
         <View style={styles.containerTitle}>
-            <Text style={styles.title}>{title}</Text>
+          <Text style={styles.title}>{title}</Text>
         </View>
         <Text style={styles.date}>{date}</Text>
         <Text style={styles.explanation}>{explanation}</Text>
         <Text style={styles.copyright}>Copyright: {copyright}</Text>
-    </ScrollView>
+      </ScrollView>
+    </GestureHandlerRootView>
   );
 };
 
