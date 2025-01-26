bcdda00cb9f076a0d2ead4f796af8f4560bc7848
Vee Pabuence Vargas <beepabuence@gmail.com>
2025-01-26 18:52:26 -0500

    Animations on pressables in infoAPOD

--- src/views/InfoAPOD/InfoAPOD.jsx
+++ src/views/InfoAPOD/InfoAPOD.jsx
@@ -4,6 +4,7 @@ import {styles} from './styles';
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
+  withSpring
 } from 'react-native-reanimated';
 import {useNavigation} from '@react-navigation/native';
 
@@ -11,19 +12,38 @@ const InfoAPOD = ({route}) => {
   const {title, url, hdurl, date, explanation, copyright} = route.params;
   const [isHD,setIsHD] = useState(false);
   const navigation = useNavigation();
+  const scaleHD = useSharedValue(1);
+  const scaleFullScreen = useSharedValue(1);
+
+  const animatedStyleHD = useAnimatedStyle(() => ({
+    transform: [{ scale: scaleHD.value }],
+  }));
+
+  const animatedStyleFullScreen = useAnimatedStyle(() => ({
+    opacity: scaleFullScreen.value
+  }));
 
   return (
     <SafeAreaView style={styles.container}>
           <View style={styles.containerImage}>
             <Animated.Image source={{uri: isHD ? hdurl : url}} style={[styles.image]} />
-            <Pressable style={styles.buttonHD} onPress={() => {
-              setIsHD(!isHD);
-            }}>
-              <Text style={styles.buttonHDText}>{isHD ? 'noHD' : 'HD'}</Text>
+            <Pressable
+              style={styles.buttonHD}
+              onPress={() => setIsHD(!isHD)}
+              onPressIn={() => (scaleHD.value = withSpring(0.8))}
+              onPressOut={() => (scaleHD.value = withSpring(1))}>
+              <Animated.View style={animatedStyleHD}>
+                <Text style={styles.buttonHDText}>{isHD ? 'noHD' : 'HD'}</Text>
+              </Animated.View>
             </Pressable>
-            <Pressable style={styles.fullScreenButton} onPress={()=> navigation.navigate('ImageScreen', {title, url, hdurl})}>
-              <Text style={styles.fullScreenText}>[  ]</Text>
-            </Pressable>
+            <Animated.View style={[styles.fullScreenButton,animatedStyleFullScreen]}>
+              <Pressable
+                onPress={() => navigation.navigate('ImageScreen', { title, url, hdurl })}
+                onPressIn={() => (scaleFullScreen.value = withSpring(0.6))}
+                onPressOut={() => (scaleFullScreen.value = withSpring(1))}>
+                  <Text style={styles.fullScreenText}>[  ]</Text>
+              </Pressable>
+            </Animated.View>
           </View>
           <View style={styles.containerTitle}>
             <Text style={styles.title}>{title}</Text>
