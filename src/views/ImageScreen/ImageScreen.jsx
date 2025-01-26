af9b23fc49484a6d735866894a66b4e88e0d3ef6
Vee Pabuence Vargas <beepabuence@gmail.com>
2025-01-26 18:12:32 -0500

    Double tap to fix scale and translateXY

--- src/views/ImageScreen/ImageScreen.jsx
+++ src/views/ImageScreen/ImageScreen.jsx
@@ -1,38 +1,87 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { Gesture, GestureDetector, GestureHandlerRootView } from 'react-native-gesture-handler';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Gesture, GestureDetector, GestureHandlerRootView } from "react-native-gesture-handler";
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
-} from 'react-native-reanimated';
+  withDecay,
+} from "react-native-reanimated";
 
-const ImageScreen = ({route}) => {
-  const {hdurl} = route.params;
+const ImageScreen = ({ route }) => {
+  const { hdurl } = route.params;
+
   const scale = useSharedValue(1);
   const savedScale = useSharedValue(1);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+  const savedTranslateX = useSharedValue(0);
+  const savedTranslateY = useSharedValue(0);
 
   const pinchGesture = Gesture.Pinch()
     .onUpdate((e) => {
       scale.value = savedScale.value * e.scale;
-      console.log(scale.value)
+      if (scale.value < 1) scale.value = 1; 
     })
     .onEnd(() => {
-      console.log(savedScale.value)
-      scale.value = savedScale.value;
+      savedScale.value = scale.value;
     });
 
-    const animatedStyle = useAnimatedStyle(() => ({
-      transform: [{ scale: scale.value }],
-    }));
+  const panGesture = Gesture.Pan()
+    .onUpdate((e) => {
+      translateX.value = savedTranslateX.value + e.translationX;
+      translateY.value = savedTranslateY.value + e.translationY;
+    })
+    .onEnd((e) => {
+      translateX.value = withDecay({ velocity: e.velocityX });
+      translateY.value = withDecay({ velocity: e.velocityY });
+      savedTranslateX.value = translateX.value;
+      savedTranslateY.value = translateY.value;
+    });
+
+    const doubleTapGesture = Gesture.Tap()
+      .numberOfTaps(2)
+      .maxDuration(250)
+      .onStart(()=>{
+        scale.value = 1;
+        savedScale.value = 1;
+        translateX.value = 0;
+        translateY.value = 0;
+        savedTranslateX.value = 0;
+        savedTranslateY.value = 0;
+      });
+
+  const composedGesture = Gesture.Simultaneous(pinchGesture, panGesture, doubleTapGesture);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [
+      { translateX: translateX.value },
+      { translateY: translateY.value },
+      { scale: scale.value },
+    ],
+  }));
 
   return (
-    <GestureHandlerRootView style={{backgroundColor: '#000'}}>
-      <GestureDetector gesture={pinchGesture}>
-        <Animated.Image source={{uri: hdurl}} style={[{width: '100%', height: '100%', objectFit: 'contain'}, animatedStyle]}/>
+    <GestureHandlerRootView style={styles.container}>
+      <GestureDetector gesture={composedGesture}>
+        <Animated.Image
+          source={{ uri: hdurl }}
+          style={[styles.image, animatedStyle]}
+          resizeMode="contain"
+        />
       </GestureDetector>
     </GestureHandlerRootView>
-    
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
 
 export default ImageScreen;
